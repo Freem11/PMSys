64459ef36fcf47fe91982e61c823ca9dd32f6532
Freem11 <freem1985@gmail.com>
2021-12-12 14:23:38 -0800

    react started

---
+++ pmtool/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+// import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+        "time to get rich"
+    </div>
+  );
+}
+
+export default App;
