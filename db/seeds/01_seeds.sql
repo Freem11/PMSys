9d57db0eb05c09569ab8884682e44a5cd5ead5d7
Freem11 <freem1985@gmail.com>
2021-12-12 12:11:57 -0800

    initial db setup

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,16 @@
+
+-- DELETE FROM users;
+-- DELETE FROM projects;
+
+-- ALTER SEQUENCE users_id_seq RESTART WITH 1;
+-- ALTER SEQUENCE projects_id_seq RESTART WITH 1;
+
+INSERT INTO users(email, password)
+VALUES ('jerry@gmail.com', 'jerry'),
+('george@gmail.com', 'george');
+
+INSERT INTO projects(name, status, user_id)
+VALUES ('manhatten', 'active', 1),
+('long island', 'active', 2),
+('queens', 'inactive', 2),
+('bronx', 'inactive', 1);
